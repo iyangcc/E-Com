d814c1a97c75012e1b0cc819633e45feddbcd145
zhangzy <931032337@qq.com>
2018-05-13 00:04:23 +0800

    initial

---
+++ src/page/index/index.js
@@ -0,0 +1,4 @@
+'use strict';
+require('../modules');
+require('./index.css');
+console.log('hello index');
---
+++ src/page/login/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var $$ = require('jquery');
+
+$$('body').html("hello login.....");
+require('../modules');
+console.log('hello login....');
